de3fd547e845cc7e5870bc60cd7f5f77e8d0462f
c5apple <c5apple_party@yahoo.co.jp>
2016-12-01 23:51:35 +0900

    TODOリスト作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+class Todo {
+  title: string;
+  desc: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private todo: Todo;
+  private todos: Todo[];
+  ngOnInit(): void {
+    this.todo = new Todo;
+    this.todos = [];
+  }
+  public onSubmit(): void {
+    this.todos.push(this.todo);
+    this.todo = new Todo;
+  }
+  public onDelete(index: number): void {
+    this.todos.splice(index, 1);
+  }
 }
